d5e4969aa408f5941f229db2c1e6116516a783f6
westonsam <132954237+westonsam@users.noreply.github.com>
2023-06-07 12:39:26 -0700

    Built Nav Component

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./HomeScreen.css";
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div className='HomeScreen'>
+        {/* Nav */}
+        <Nav />
+        {/* Banner */}
+
+        {/* Row */}
+    </div>
+  )
+}
+
+export default HomeScreen
