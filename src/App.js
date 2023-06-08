20cc8276ec9c433eb4547b1953f2f21176cdf63c
westonsam <132954237+westonsam@users.noreply.github.com>
2023-06-07 22:29:46 -0700

    Integrated TMDB API (with Banner)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
+import "./App.css";
 import HomeScreen from './HomeScreen';
+
 function App() {
   return (
     <div className="app">
--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,25 @@
-import React from 'react'
-import "./Banner.css"
+import React, { useEffect, useState } from 'react';
+import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData();
+    }, []);
+
     // n = number of characters to include in string before truncating
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
@@ -10,16 +28,20 @@ function Banner() {
     return (
         <header className='banner' style={{
             backgroundSize: "cover",
-            backgroundImage: `url('https://th.bing.com/th/id/OIP.zqzGylXzPLtaWKSD2xOtyAHaEo?pid=ImgDet&rs=1')`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
         }}>
             <div className='banner__contents'>
-                <h1 className='banner__title'>Movie Name</h1>
+                <h1 className='banner__title'>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
                 <div className='banner__buttons'>
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
-                <h1 className='banner__description'>{truncate(`This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`, 200)}</h1>
+                <h1 className='banner__description'>
+                    {truncate(movie?.overview,200)}
+                </h1>
             </div>
 
             <div className='banner--fadeBottom' />
