2ff6a7efcaa07655608d255aa4023605a07577dc
westonsam <132954237+westonsam@users.noreply.github.com>
2023-06-08 12:31:45 -0700

    Built Login Screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./App.css";
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen'
 import {
   BrowserRouter,
   Routes,
@@ -8,13 +9,18 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          {/* <Route path="users/*" element={<Users />} /> */}
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            {/* <Route path="users/*" element={<Users />} /> */}
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
